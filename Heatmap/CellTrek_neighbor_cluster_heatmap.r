28bc9c0bf95c962228a9efd57131ceb58bec28a2
cmo <c.mo@wustl.edu>
2022-06-24 00:09:44 -0500

    modified heatmap script

--- Heatmap/CellTrek_neighbor_cluster_heatmap.r
+++ Heatmap/CellTrek_neighbor_cluster_heatmap.r
@@ -4,14 +4,10 @@
 #### DEMO #####
 # obj_use: Plot object from "PrepareSpatialPlotObject"
 # sn_use: the sc/snRNA object
-# cell_type_plt = 'immune'
-# GetExpressionData(obj_use, sn_use, cell_type = cell_type_plt) %>% 
-#     GetTopGeneMatrix %>% 
-#     MakeClusterHeatmap
+# MakeClusterHeatmap(obj_use, sn_use, assay = 'SCT', n_gene = 100)
 
-
-# 1. expression subgroup
-GetExpressionData = function(plot_obj, sc_obj, assay = 'SCT', cell_type){
+## [Internal] 1. expression subgroup 
+GetExpressionData = function(plot_obj, sc_obj, assay = 'SCT'){
     # Use MakeNames on scRNA to match that in cellTrek
     sc_obj = RenameCells(sc_obj, new.names = colnames(sc_obj) %>% make.names())
     
@@ -38,36 +34,44 @@ GetExpressionData = function(plot_obj, sc_obj, assay = 'SCT', cell_type){
     exp_df_plot
 }
 
-## 2 Select genes to plot
-## Idea A: Top n most variable genes
+##  [Internal]  2 Select genes to plot - Top n most variable genes
 GetTopGeneMatrix = function(expr_matrix, n_gene = 100){
     top_var_genes = apply(expr_matrix, 2, var, na.rm = T) %>% 
         .[.>0] %>% # filter out var 0 genes
         sort(., decreasing = T) %>% 
         .[1:n_gene] %>% names # Top n genes
-    expr_matrix = expr_matrix[,top_var_genes] # column is ordered by variance of gene
+    expr_matrix[,top_var_genes] # column is ordered by variance of gene
 }
 
 ## 3. Plot
-MakeClusterHeatmap = function(expression_matrix, gene_to_label){
-    ## Gene to mark
+MakeClusterHeatmap = function(plot_obj, sc_obj, assay = 'SCT', n_gene = 100, gene_to_label){
+    ## Get Expression mtx
+    expression_matrix = GetExpressionData(plot_obj, sc_obj, assay)
+    expression_matrix = GetTopGeneMatrix(expression_matrix, n_gene) 
+    
+    ### PLOT
+    # 1. Gene to mark
     if(missing(gene_to_label)){
         message('By default label top most variable genes')
         gene_to_label = colnames(expression_matrix)[1:20] # Matrix ordered. Top 20 most variable genes
     }
     
-    # Make Gene label marks
+    # 2. Column split
+    plot_meta = plot_obj$Neighbor_cluster_table %>% 
+        column_to_rownames('Origin') %>% .[rownames(expression_matrix), ]
+    
+    # 3. Make Gene label marks
     gene_mark  = intersect(names(expression_matrix), gene_to_label)
-    gene_mark_pos = match(gene_mark, names(exp_top_df))
+    gene_mark_pos = match(gene_mark, names(expression_matrix))
     right_anno = rowAnnotation(foo = anno_mark(at = gene_mark_pos, labels = gene_mark))
 
-    ## PLOT
-    Heatmap(exp_top_df %>% scale %>% t,  right_annotation = right_anno,
+    ## 4. PLOT
+    Heatmap(expression_matrix %>% scale %>% t,  right_annotation = right_anno,
             name = 'Scaled\nExpression',
             show_column_names = F,
             show_row_names = F,
-                show_row_dend = F,
-            column_split = sn_celltrek_plot_meta$Cell_cluster,
+            show_row_dend = F,
+            column_split = plot_meta$Cell_cluster,
                 cluster_column_slices = F
             )
 }
