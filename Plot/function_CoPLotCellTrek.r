2bf0331cbeac67dfa9e5eabb68739b70044f3922
cmo <c.mo@wustl.edu>
2022-12-19 15:50:44 -0600

    fix coplot function

--- Plot/function_CoPLotCellTrek.r
+++ Plot/function_CoPLotCellTrek.r
@@ -1,7 +1,7 @@
 ### Plot ST and CellTrek together 
 CoPlotSTCellTrek = function(celltrek_obj, st_obj, ptsize = 0.1, stptsize = 1){
     # Make table
-    ST_table = st_obj@images$U1@coordinates[,c('imagerow','imagecol')] %>% 
+    ST_table = st_obj@images[[1]]@coordinates[,c('imagerow','imagecol')] %>% 
         setNames(c('coord_x','coord_y')) %>% 
         mutate(assay='ST')
     
@@ -12,8 +12,8 @@ CoPlotSTCellTrek = function(celltrek_obj, st_obj, ptsize = 0.1, stptsize = 1){
     
     # Plot
     ggplot(data.frame(), aes(x = coord_y, y = coord_x, color = assay)) + 
+        geom_point(data = ST_table, size = stptsize, alpha = 0.3) + 
         geom_point(data = celltrek_table, size = ptsize) + 
-        geom_point(data = ST_table, size = stptsize) + 
         cowplot::theme_cowplot() + 
         coord_fixed()
 }
